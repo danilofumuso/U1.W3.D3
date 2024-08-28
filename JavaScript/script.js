16c985cee562bd87084f41be2e6644ea7780c87c
Danilo Fumuso <danilofumuso@gmail.com>
2024-08-28 17:40:06 +0200

    U1.W3.D3 pratica JS completo

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,4 @@
-const firstDiv = document.querySelector("body > div");
+/* const firstDiv = document.querySelector("body > div");
 
 const addButton = document.createElement("button");
 addButton.type = "button";
@@ -9,8 +9,35 @@ firstDiv.appendChild(addButton);
 
 const oList = document.createElement("ol");
 oList.classList.add("ordered-list");
-oList.innerText = "To do list";
+
 
 firstDiv.appendChild(oList);
+ */
 
-const pushText = function () {};
+const addButton = document.getElementById("task-button");
+
+const liMaker = function () {
+  const input = document.getElementById("task-input");
+  const inputValue = input.value;
+  if (inputValue !== "") {
+    const li = document.createElement("li");
+    li.innerText = inputValue;
+    const delButton = document.createElement("button");
+    delButton.classList.add("task-delete");
+    delButton.innerText = "❌";
+    delButton.addEventListener("click", function () {
+      li.remove();
+    });
+    li.appendChild(delButton);
+    li.addEventListener("click", function () {
+      li.classList.toggle("strike");
+    });
+    const ol = document.getElementById("task-list");
+    ol.appendChild(li);
+  } else {
+    alert("Devi inserire del testo!");
+    input.focus(); //con questo forzo l'ingresso nell'input!
+  }
+};
+
+addButton.addEventListener("click", liMaker);
